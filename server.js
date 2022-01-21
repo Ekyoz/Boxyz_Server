56aaa5261829b7e801f45143cba62cf98aa0ef6f
alexandre <alexandre.tresallet@gmail.com>
2022-01-21 22:08:11 +0100

    add server nodeJs

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+// ajout de socket.io
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'))
+app.get('/', function (req, res) {
+    res.sendFile('index.html', { root: __dirname })
+})
+
+app.get('/json', function (req, res) {
+    res.status(200).json({ "message": "ok" })
+})
+
+// établissement de la connexion
+io.on('connection', (socket) => {
+    console.log(`Connecté au client ${socket.id}`)
+})
+
+io.
+
+    // on change app par server
+    server.listen(3000, function () {
+        console.log('Votre app est disponible sur localhost:3000 !')
+    })
